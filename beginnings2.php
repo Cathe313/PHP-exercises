17873b3127dc0489dc91a4fd17f13bd2489093a2
Catherine <ducharme.catherine@gmail.com>
2015-12-29 20:00:06 +0000

    Exercie 2: Defining variables (twinkle twinkle little star)

---
+++ beginnings2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <?php 
+        echo 'Twinkle, Twinkle little star.</br>';
+        $twinkle = 'Twinkle';
+        $star = 'star';
+        echo "$twinkle, $twinkle little $star. </br>";
+        $twinkle = 'Miaw';
+        $star = 'cat';
+        echo "$twinkle, $twinkle little $star. </br>";
+        ?>
+    </body>
+</html>
