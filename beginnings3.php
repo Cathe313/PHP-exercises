412e65d839aa2047b0d029f4c89a77ea135e20dc
Catherine <ducharme.catherine@gmail.com>
2015-12-29 20:07:47 +0000

    Exercise 3: arithmetic operators

---
+++ beginnings3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <?php
+            $x=10;
+            $y=7;
+            echo "$x + $y = ".($x + $y)."</br>";
+            echo "$x - $y = ".($x - $y)."</br>";
+            echo "$x * $y = ".($x * $y)."</br>";
+            echo "$x / $y = ".($x / $y)."</br>";
+            echo "$x % $y = ".($x % $y)."</br>";
+        ?>
+    </body>
+</html>
