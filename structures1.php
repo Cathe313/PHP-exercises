484ebfacdd1b5d8d09eb81c3f9dbb7072db1d844
Catherine <ducharme.catherine@gmail.com>
2015-12-29 22:52:25 +0000

    Created Beginnings and Control Structures dirs, did ex.1 (Conditional statements) in new series

---
+++ structures1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Conditional Statements</title>
+    </head>
+    <body>
+        <?php
+            $month = date('F', time());
+            if ($month == 'August') {
+                echo "<p>It's August, so it's really hot.</p>";
+            } else {
+                echo "<p>Not August, so at least not in the peak of the heat.</p>";
+            }
+        ?>
+    </body>
+</html>
