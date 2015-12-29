f0e63ba975a52f1651b347ec784af44178c9d30c
Catherine <ducharme.catherine@gmail.com>
2015-12-29 22:26:20 +0000

    Ex. 5, var_dump

---
+++ beginnings5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Destruction!</title>
+    </head>
+    <body>
+        <?php
+            $name = 'Harry';
+            $age = 28;
+            var_dump($name);
+            echo "<br/>";
+            print($name);
+            echo "<br/>";
+            var_dump($age);
+            $age = NULL;
+            var_dump($age);
+        ?>
+    </body>
+</html>
