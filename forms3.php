79aaeb46524e3c08a3a7b85ec1e3d2b65302b288
Catherine <ducharme.catherine@gmail.com>
2015-12-31 17:48:27 +0000

    Redid ex. 3 with if-elseif-else statement

--- forms3.php
+++ forms3.php
@@ -14,28 +14,20 @@
             </form>';
         } else {
             $day = strtolower($_POST["day"]);
-            //If-elseif-else construction was way too awkward.
-            switch ($day) {
-                case 'monday':
-                    echo '<p>Laugh on Monday, laugh for danger.</p>';
-                    break;
-                case 'tuesday':
-                    echo '<p>Laugh on Tuesday, kiss a stranger.</p>';
-                    break;
-                case 'wednesday':
-                    echo '<p>Laugh on Wednesday, laugh for a letter.</p>';
-                    break;
-                case 'thursday':
-                    echo '<p>Laugh on Thursday, something better.</p>';
-                    break;
-                case 'friday':
-                    echo '<p>Laugh on Friday, laugh for sorrow.</p>';
-                    break;
-                case 'saturday':
-                    echo '<p>Laugh on Saturday, joy tomorrow.</p>';
-                    break;
-                default:
-                    echo '<p>Don\'t think you spelled that right. Also, no Sundays allowed.</p>';
+            if ($day == 'monday') {
+                echo '<p>Laugh on Monday, laugh for danger.</p>';
+            } elseif ($day == 'tuesday') {
+                echo '<p>Laugh on Tuesday, kiss a stranger.</p>';
+            } elseif ($day == 'wednesday') {
+                echo '<p>Laugh on Wednesday, laugh for a letter.</p>';
+            } elseif ($day == 'thursday') {
+               echo '<p>Laugh on Thursday, something better.</p>'; 
+            } elseif ($day == 'friday') {
+                echo '<p>Laugh on Friday, laugh for sorrow.</p>';
+            } elseif ($day == 'saturday') {
+                echo '<p>Laugh on Saturday, joy tomorrow.</p>';
+            } else {
+                echo '<p>Don\'t think you spelled that right. Also, no Sundays allowed.</p>';
             }
         }
         
